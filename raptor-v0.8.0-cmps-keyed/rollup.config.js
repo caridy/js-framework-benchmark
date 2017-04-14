28fe6fd773dbb49218adde057240f7d35dbc5434
Diego Ferreiro Val <elfogris@gmail.com>
2017-04-14 14:19:55 -0700

    Upgrade build proces + Upgrade version

--- raptor-v0.8.0-cmps-keyed/rollup.config.js
+++ raptor-v0.8.0-cmps-keyed/rollup.config.js
@@ -1,14 +1,8 @@
 /* jshint node: true */
 
-/**
- * This file builds the browser version in dist/ folder.
- */
-
 const p = require('path');
 const babel = require('rollup-plugin-babel');
 const nodeResolve = require('rollup-plugin-node-resolve');
-const includePaths = require('rollup-plugin-includepaths');
-const commonjs = require('rollup-plugin-commonjs');
 const uglify = require('rollup-plugin-uglify');
 const strip = require('rollup-plugin-strip');
 const raptor = require('rollup-plugin-raptor-compiler');
@@ -25,27 +19,9 @@ module.exports = {
         raptor(),
         babel({
             babelrc: false,
-            presets: [
-                [
-                    "es2015",
-                    {
-                        "modules": false
-                    }
-                ]
-            ],
+            presets: [["env", { "modules": false }]]
         }),
-        includePaths({
-            include: {
-                'x:app': './src/app.js',
-                'x:row': './src/row.js',
-            },
-        }),
-        nodeResolve({
-            module: true,
-        }),
-        commonjs({
-            sourceMap: true,
-        }),
+        nodeResolve({ module: true }),
         isProduction && strip({
             debugger: true,
             functions: [ 'console.*', 'assert.*' ],
