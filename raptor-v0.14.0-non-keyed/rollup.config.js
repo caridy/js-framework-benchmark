e325c321923736c7781c55b1480c5e29805871cb
Caridy <cpatino@salesforce.com>
2017-08-15 14:07:39 -0400

    snapshot for 0.13 after running all benchmarks

--- raptor-v0.14.0-non-keyed/rollup.config.js
+++ raptor-v0.14.0-non-keyed/rollup.config.js
@@ -3,10 +3,17 @@
 const path = require('path');
 const rollupUglify = require('rollup-plugin-uglify');
 const es6Uglify = require('uglify-js-harmony');
+const strip = require('rollup-plugin-strip');
 const raptorCompiler = require('rollup-plugin-raptor-compiler');
 const isProduction = process.env.NODE_ENV === 'production';
 
-const plugins = [raptorCompiler()];
+const plugins = [
+    raptorCompiler(),
+    strip({
+        debugger: true,
+        functions: [ 'console.*', 'assert.*' ],
+    }),
+];
 
 if (isProduction) {
     plugins.push(rollupUglify(
